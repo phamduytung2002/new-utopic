465fc4e3bfcfa6a56e9f378fdec77b0c0574f5ab
PDT <phamduytung2002@gmail.com>
2025-04-06 23:51:30 +0700

    update

--- contrastive_topic_model/run_news.py
+++ contrastive_topic_model/run_news.py
@@ -1,21 +1,21 @@
 import os
 
-bsz = 128
+bsz = 64
 dataset = 'news'
-n_topic = 50
+n_topic = 10
 epochs_1 = 100
 epochs_2 = 100
 bert_model = 'sentence-transformers/all-MiniLM-L6-v2'
 
 if __name__ == "__main__":
-    for stage_1_dist in [0.1, 0.5, 1, 5, 10, 50, 100]:
+    for stage_1_dist in [1]:
         print("*******************************")
         print(f'Running {dataset} with stage_1_dist={stage_1_dist}')
         os.system(f'python runs1.py --base-model {bert_model} --dataset {dataset} --n-word 5000 --epochs-1 {epochs_1} --bsz {bsz} --coeff-1-dist {stage_1_dist} --n-cluster {n_topic}')
 
-    for stage_1_dist in [0.1, 0.5, 1, 5, 10, 50, 100]:
-        for stage_2_cons in [0.1, 0.5, 1, 5, 10]:
-            for stage_2_dist in [0.1, 0.5, 1, 5, 10]:
+    for stage_1_dist in [1]:
+        for stage_2_cons in [1]:
+            for stage_2_dist in [1]:
                 print("*******************")
                 print("Running stage 2 with:")
                 print(f"stage_2_cons={stage_2_cons}")
